9ac6a4a6029e006a207108dd269ba8f75662a7c8
Lex Baldwin <starwarbler@gmail.com>
2023-06-10 20:54:10 -0500

    pushing updated working version for week 13 assignment

--- src/main/java/pet/store/entity/Customer.java
+++ src/main/java/pet/store/entity/Customer.java
@@ -1,11 +1,31 @@
 package pet.store.entity;
 
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
 @Entity
 @Data
 public class Customer {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long customer_id;
+	private String customer_first_name;
+	private String customer_last_name;
+	private String customer_email;
 	
+	@ManyToMany(mappedBy = "customers", cascade = CascadeType.PERSIST)
+	@EqualsAndHashCode.Exclude
+	@ToString.Exclude
+	private Set<PetStore> petStores = new HashSet<>();
 
 }
--- src/main/java/pet/store/entity/PetStore.java
+++ src/main/java/pet/store/entity/PetStore.java
@@ -1,11 +1,32 @@
 package pet.store.entity;
 
-import jakarta.persistence.Entity;
+import java.util.HashSet;
+import java.util.Set;
+
+import jakarta.persistence.*;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 
 @Entity
 @Data
 public class PetStore {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long pet_store_id;
+	private String pet_store_name;
+	private String pet_store_address;
+	private String pet_store_city;
+	private String pet_store_state;
+	private String pet_store_zip;
+	private String pet_store_phone;
 	
+	@ManyToMany(cascade = CascadeType.PERSIST)
+	@JoinTable(name = "pet_store_customer", 
+		joinColumns = @JoinColumn(name ="pet_store_id"),
+		inverseJoinColumns = @JoinColumn(name = "customer_id"))
+	@EqualsAndHashCode.Exclude
+	@ToString.Exclude
+	private Set<Customer> customers = new HashSet<>();
 
 }
